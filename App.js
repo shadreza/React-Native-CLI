ee21361934be80965cc320e61941a664d019ad18
shadreza <shadreza100@gmail.com>
2022-12-26 13:05:21 +0600

    The CLI project has been initialized

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
