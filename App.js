937091d7ff7a2c6716ec8fdf0151c6abdfbface3
shadreza <shadreza100@gmail.com>
2022-12-26 17:07:34 +0600

    The layouts are comming

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 // eslint-disable-next-line prettier/prettier
-import { StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <View />
-      <View />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder="Your Course Goal!" />
+        <Button title="Add a Goal" />
+      </View>
+      <View>
+        <Text>List of goals ...</Text>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 50,
+  },
+});
 
 export default App;
