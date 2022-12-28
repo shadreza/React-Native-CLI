c7500ce971d4b3f2d23afea9e4fa84a88775137e
shadreza <shadreza100@gmail.com>
2022-12-28 21:12:55 +0600

    Keyboard was implemented for number-pad style... Also shift + command + k was useful

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,7 +6,13 @@ import PrimaryButton from '../components/Buttons/PrimaryButton';
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.numberInput} maxLength={2} />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Hi</PrimaryButton>
       <PrimaryButton>Kemon Acho</PrimaryButton>
     </View>
