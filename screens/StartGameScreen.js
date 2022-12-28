8e308c6f060ef7ee1d633932a896865e728b970a
shadreza <shadreza100@gmail.com>
2022-12-28 14:41:18 +0600

    The Start Game Screen has been initialized

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import { StyleSheet, TextInput, View } from 'react-native';
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      {/* Buttons */}
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({});
